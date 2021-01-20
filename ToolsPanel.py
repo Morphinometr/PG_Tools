28d4fdf33d706100c41bd37e8b987cf52dfbd999
Kharkovschenko Konstantin <lightmap.kharkovshenko@gmail.com>
2021-01-20 16:49:03 +0300

    Optimize add

--- ToolsPanel.py
+++ ToolsPanel.py
@@ -1,4 +1,5 @@
 import bpy
+from bpy.types import (Panel)
 bl_info = {"name": "Tools Panel",
            "description": "tools",
            "author": "Morphin",
@@ -13,17 +14,35 @@ bl_info = {"name": "Tools Panel",
     
     
 
+class MESH_OT_Optimize(bpy.types.Operator):
+    bl_label = "Optimize"
+    bl_idname = "mesh.optimize"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    @classmethod
+    def poll(cls, context):
+        return context.area.type == 'VIEW_3D' and context.active_object.type == 'MESH'
+            
+            
+    
+    def execute(self, context):
+        
+        bpy.ops.object.modifier_add(type='DECIMATE')
+        bpy.context.object.modifiers["Decimate"].decimate_type = 'DISSOLVE'
+        bpy.ops.object.modifier_apply(modifier="Decimate", report=True)
+
+        
+        return {'FINISHED'}
 
 
-
-class VIEW3D_PT_Pixel(bpy.types.Panel):
+class VIEW3D_PT_Pixel(Panel):
     """Creates a Panel in the scene context of the 3D view N panel"""
     
-    bl_label = "Pixel"
+    bl_label = "Model"
     bl_idname = "VIEW3D_PT_Pixel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = "Tool"
+    bl_category = "Pixel"
     
     
     def draw(self, context):
@@ -32,16 +51,13 @@ class VIEW3D_PT_Pixel(bpy.types.Panel):
         
         row = column.row(align=True)
         
+        column.operator("mesh.optimize", text='Optimize')
         
                 
-        column.prop(context.preferences.inputs, "use_rotate_around_active")
-        
-        column.prop(context.preferences.edit, "use_mouse_depth_cursor")
-        
-        column.prop(context.preferences.inputs, "use_mouse_emulate_3_button")
         
 classes = (
     VIEW3D_PT_Pixel,
+    MESH_OT_Optimize
     
     )
                     
@@ -55,3 +71,5 @@ def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
 
+if __name__ == "__main__":
+    register()
