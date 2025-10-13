e82439b4fd1fc726108ec565d8e21d36ac435fd9
Kharkovshchenko Konstantin <k.kharkovshchenko@cubicgames.com>
2025-10-13 15:48:34 +0400

    version bump

--- __init__.py
+++ __init__.py
@@ -52,7 +52,7 @@ from bpy.types import (
 bl_info = {
     "name": "PG Tools",
     "author": "Kharkovschenko Konstantin",
-    "version": (0, 3, 1),
+    "version": (0, 3, 2),
     "blender": (4, 0, 0),
     "description": "PixelGun pipeline tools",
     "location": "Addons Preferences",
